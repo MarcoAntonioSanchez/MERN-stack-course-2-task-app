6ba620501a59710f36929b49d429d17c42099269
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2024-04-28 10:29:51 -0600

    7. MongoDB setup connection method with alternative practice done, setting the first MongoDB Task schema / model

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
+require('dotenv').config() // Remove for production
 const express = require("express");
+const connectDB = require("./config/connectDB");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -7,8 +10,28 @@ app.get("/", (req, res)=> {
     res.send("Home Page");
 })
 
-
 const PORT  = process.env.PORT || 5000; // Use of environment variables for port number, or, directly 5000
-app.listen(PORT, ()=> { // Now the PORT variable contain's the port number.
-    console.log(`Server running on port ${PORT}`);
-});
+
+// One way to connect to mongoDB
+mongoose
+.connect(process.env.MONGO_URI)
+.then(()=> {
+    app.listen(PORT, ()=> { // Now the PORT variable contain's the port number.
+        console.log(`Server running on port ${PORT}`);
+    })
+})
+.catch((err) => console.log(err));
+
+// Alternative way to connect to mongoDB
+// const startServer = async () => {
+//     try {
+//         await connectDB()
+//         app.listen(PORT, ()=> { // Now the PORT variable contain's the port number.
+//             console.log(`Server running on port ${PORT}`);
+//         });
+//     } catch (error) {
+//         console.log("RVN - Error detected on server start:");
+//         console.log(error);
+//     }
+// }
+// startServer();
